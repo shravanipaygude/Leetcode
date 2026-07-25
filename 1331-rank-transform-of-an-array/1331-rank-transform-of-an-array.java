fa887b7205af3cd516253a0a728eea2f0e9ac2c0
shravanipaygude <sidhipaygude@gmail.com>
2026-07-25 17:21:01 +0530

    Time: 33 ms (36.99%), Space: 75.5 MB (55.54%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sortedArr = arr.clone();
+        Arrays.sort(sortedArr);
+
+        Map<Integer, Integer> rankMap = new HashMap<>();
+        int rank = 1;
+
+        for (int num : sortedArr) {
+            if (!rankMap.containsKey(num)) {
+                rankMap.put(num, rank++);
+            }
+        }
+
+        int[] result = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            result[i] = rankMap.get(arr[i]);
+        }
+
+        return result;
+    }
+}
